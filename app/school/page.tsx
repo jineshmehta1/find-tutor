bef47d7bb4fdc2f5d1b7c193f6cdef5f2140f9a9
unknown <mehtajinesh977@gmail.com>
2026-01-04 12:55:51 +0530

    cha

--- app/school/page.tsx
+++ app/school/page.tsx
@@ -102,15 +102,17 @@ export default async function PrimarySchoolPage() {
         <div className="absolute inset-0 z-10 bg-amber-500/35 " />
 
         {/* TEXT CONTENT (White & Bold) */}
-        <div className="relative z-20 container mx-auto px-8 md:px-16">
-          <div className="max-w-4xl">
-            <h1 className="text-white text-2xl md:text-5xl text-semibold uppercase tracking-tight drop-shadow-md mb-2">
-              Aacharya Pre Primary School
-            </h1>
-            <p className="text-white text-sm md:text-3xl font-medium leading-tight drop-shadow-sm opacity-95">
+        <div className="absolute inset-0 z-10 bg-yellow-400/70 md:bg-yellow-400/60 mix-blend-multiply pointer-events-none" />
+
+        {/* Content */}
+        <div className="absolute inset-0 z-20 flex flex-col items-center justify-center px-6 text-center">
+          <h1 className="text-white text-4xl md:text-7xl  leading-[1.1] mb-10 drop-shadow max-w-5xl">
+            Aacharya <br className="hidden md:block" /> Pre Primary School
+          </h1>
+
+           <p className="text-white text-sm md:text-3xl font-medium leading-tight drop-shadow-sm opacity-95">
               Learning with Love, Growing with Confidence
             </p>
-          </div>
         </div>
       </section>
 

6d3d1e502a81b5ebf3a7bd71eb5c45ecc211f09f
unknown <mehtajinesh977@gmail.com>
2026-01-21 16:25:41 +0530

    ch

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -120,7 +120,7 @@ export default async function HomeHero() {
                     />
                   </div>
 
-                  <h3 className="text-[12px] md:text-[15px] font-black text-[#332a3d] mb-2 md:mb-4 uppercase leading-tight">
+                  <h3 className="text-[12px] md:text-[19px] font-black text-amber-500 mb-2 md:mb-4 uppercase leading-tight">
                     {program.label}
                   </h3>
 
